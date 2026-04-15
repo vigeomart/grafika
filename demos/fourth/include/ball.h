a9e9d1974ee33cfc8ee9e398022c9f3dae2f317c
vigeomart <martin.veres@student.uni-miskolc.hu>
2026-04-15 14:35:18 +0200

    feat:  kattintassal athelyezheto, falak mukodnek, pageup és pagedown r növelése

---
+++ demos/fourth/include/ball.h
@@ -0,0 +1,34 @@
+#ifndef BALL_H
+#define BALL_H
+
+/**
+ * Ball position and speed
+ */
+typedef struct Ball
+{
+    float x;
+    float y;
+    float radius;
+    float speed_x;
+    float speed_y;
+} Ball;
+
+/**
+ * Move the ball to the given position.
+ */
+void init_ball(Ball* ball, float x, float y);
+
+void init_ball_radius(Ball* ball, float r);
+
+/**
+ * Update the ball.
+ * @param time elapsed time in seconds.
+ */
+void update_ball(Ball* ball, double time);
+
+/**
+ * Render the ball.
+ */
+void render_ball(Ball* ball);
+
+#endif /* BALL_H */
---
+++ demos/fourth/src/ball.c
@@ -0,0 +1,49 @@
+#include "ball.h"
+
+#include <GL/gl.h>
+
+#include <math.h>
+
+void init_ball(Ball* ball, float x, float y)
+{
+    ball->x = x;
+    ball->y = y;
+    ball->radius = 50;
+    ball->speed_x = 350;
+    ball->speed_y = 350;
+}
+
+void init_ball_radius(Ball* ball, float r){
+    if (r > 10 && r < 100)
+    {
+        ball->radius = r;
+    }
+}
+
+void update_ball(Ball* ball, double time)
+{
+    ball->x += ball->speed_x * time;
+    ball->y += ball->speed_y * time;
+}
+
+void render_ball(Ball* ball)
+{
+    double angle;
+    double x;
+    double y;
+
+    glPushMatrix();
+    glTranslatef(ball->x, ball->y, 0.0);
+    glBegin(GL_TRIANGLE_FAN);
+    glColor3f(1.0, 0.9, 0.8);
+    glVertex2f(0, 0);
+    angle = 0;
+    while (angle < 2.0 * M_PI + 1) {
+        x = cos(angle) * ball->radius;
+        y = sin(angle) * ball->radius;
+        glVertex2f(x, y);
+        angle += 0.1;
+    }
+    glEnd();
+    glPopMatrix();
+}
